d27e6067f0f5b064137030d50c50f8e0860d4818
“Murat <mnesiren14@gmail.com>
2024-03-13 20:42:05 -0500

    changed nstance_tenancy to instance_tenancy

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "test" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  nstance_tenancy = "default"
+  instance_tenancy = "default"
 
   tags = {
     Name = "test"
